901355afcdf04628541a5a45af43423a1012674b
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-09-14 12:00:47 +0900

    up

--- lib/aws-cdk-config-automation-stack.ts
+++ lib/aws-cdk-config-automation-stack.ts
@@ -1,16 +1,83 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { ScopedAws } from "aws-cdk-lib";
+import { aws_config as config } from "aws-cdk-lib";
+import { aws_ssm as ssm } from "aws-cdk-lib";
+import { aws_iam as iam } from "aws-cdk-lib";
+import { aws_ssmincidents as ssmincidents } from "aws-cdk-lib";
 
 export class AwsCdkConfigAutomationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { accountId } = new ScopedAws(this);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkConfigAutomationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3AccessPolicy = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["s3:*"],
+          resources: ["*"],
+        }),
+      ],
+    });
+
+    const ssmAutomationRole = new iam.Role(this, "testSsmAutomationRole", {
+      roleName: "TestSsmAutomationRole",
+      assumedBy: new iam.ServicePrincipal("ssm.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonSSMAutomationRole"
+        ),
+        // iam.ManagedPolicy.fromAwsManagedPolicyName("AWSXrayWriteOnlyAccess"),
+      ],
+      inlinePolicies: {
+        s3AccessPolicy,
+      },
+    });
+
+    const securityGroupConfigRule = new config.ManagedRule(
+      this,
+      "SecurityGroupRestrictedIncoming",
+      {
+        identifier:
+          config.ManagedRuleIdentifiers
+            .EC2_SECURITY_GROUPS_RESTRICTED_INCOMING_TRAFFIC,
+        inputParameters: {
+          blockedPort1: 20,
+          blockedPort2: 21,
+          blockedPort3: 3389,
+          blockedPort4: 3306,
+          blockedPort5: 4333,
+        },
+
+        // default is 24 hours
+        // maximumExecutionFrequency:
+        // config.MaximumExecutionFrequency.TWELVE_HOURS,
+      }
+    );
+    const ssmAutomationProperty: ssmincidents.CfnResponsePlan.SsmAutomationProperty =
+      {
+        documentName: "documentName",
+        roleArn: ssmAutomationRole.roleArn,
+
+        // the properties below are optional
+        documentVersion: "documentVersion",
+        dynamicParameters: [
+          {
+            key: "key",
+            value: {
+              variable: "variable",
+            },
+          },
+        ],
+        parameters: [
+          {
+            key: "key",
+            values: ["values"],
+          },
+        ],
+        targetAccount: "targetAccount",
+      };
   }
+  // const securityGroupConfigRuleAutomation = new ssm.
 }
