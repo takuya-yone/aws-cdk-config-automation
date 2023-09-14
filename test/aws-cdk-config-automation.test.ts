4ecd3d4b5bdb6fd44015e14408bd1772191c93b1
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-09-14 11:20:50 +0900

    up

---
+++ test/aws-cdk-config-automation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkConfigAutomation from '../lib/aws-cdk-config-automation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-config-automation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkConfigAutomation.AwsCdkConfigAutomationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
